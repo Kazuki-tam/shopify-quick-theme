fd463245fd04c7ddea7bfc51434c922d69cdb6f9
yonemoto <yonemoto@tam-tam.co.jp>
2022-03-13 14:02:02 +0900

    Add: e2e

---
+++ playwright.config.ts
@@ -0,0 +1,24 @@
+// playwright.config.ts
+import { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  // Look for test files in the "tests" directory, relative to this configuration file
+  testDir: 'e2e',
+
+  // Each test is given 30 seconds
+  timeout: 30000,
+
+  // Forbid test.only on CI
+  forbidOnly: !!process.env.CI,
+
+  // Two retries for each test
+  retries: 2,
+
+  // Limit the number of workers on CI, use default locally
+  workers: process.env.CI ? 2 : undefined,
+
+  use: {
+    // Configure browser and context here
+  },
+};
+export default config;
