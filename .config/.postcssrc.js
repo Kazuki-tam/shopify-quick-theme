cd2b09f7fb40be650b301a26580e0b9d815e73bc
Kazuki.Dev <rice.book.spin0930es@gmail.com>
2024-03-23 01:36:37 +0900

    Update: packages

--- .config/.postcssrc.js
+++ .config/.postcssrc.js
@@ -4,7 +4,6 @@ module.exports = () => ({
   map: false,
   plugins: {
     'postcss-flexbugs-fixes': true,
-    'postcss-combine-media-query': true,
     'autoprefixer': {
       grid: "autoplace",
     },
